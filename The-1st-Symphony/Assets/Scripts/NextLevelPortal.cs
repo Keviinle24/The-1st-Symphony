063f8835ba7d3106e86ed855c19f655f1f9176ad
CP <125774593+ChaezP@users.noreply.github.com>
2024-07-04 23:21:00 -1000

    TestScene

--- The-1st-Symphony/Assets/Scripts/NextLevelPortal.cs
+++ The-1st-Symphony/Assets/Scripts/NextLevelPortal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class Level2 : MonoBehaviour
+public class NextLevel : MonoBehaviour
 {
 
 
@@ -14,4 +14,14 @@ public class Level2 : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+
+    public float rotationSpeed = 50f; 
+
+   // comment out if no want to spin (to comment out use //)
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
 }
+
+
