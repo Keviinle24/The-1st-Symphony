3e708ec10047c3cbd3b99f015aa7d0fbdc43aa29
CP <125774593+ChaezP@users.noreply.github.com>
2024-07-04 20:23:52 -1000

    Added sounds and next level "portal"

--- The-1st-Symphony/Assets/Scripts/MusicalPlat.cs
+++ The-1st-Symphony/Assets/Scripts/MusicalPlat.cs
@@ -7,12 +7,28 @@ public class MusicalPlat : MonoBehaviour
 
     [SerializeField] private AudioSource[] audioplayers;
 
+    private bool isAudioPlaying = false;
 
     public void OnCollisionEnter2D(Collision2D collision)
 {
-    if(collision.gameObject.tag == "Player"){
-        audioplayers[0].Play();
+    if (collision.gameObject.CompareTag("Player") && audioplayers != null && audioplayers.Length > 0)
+    {
+        //check if the audio is not already playing
+        if (!isAudioPlaying)
+        {
+            isAudioPlaying = true;
+            
+            audioplayers[0].Play();
+            
+            StartCoroutine(WaitForAudioFinish(audioplayers[0]));
+        }
     }
 }
 
+private IEnumerator WaitForAudioFinish(AudioSource audioSource)
+{
+    yield return new WaitWhile(() => audioSource.isPlaying);
+    
+    isAudioPlaying = false;
+}
 }
