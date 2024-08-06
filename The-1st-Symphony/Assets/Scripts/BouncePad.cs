07e01bbde9ec8f77c5e0f38ef00dfe519b9095d4
Ayadraeeini <aalraeeini@csuchico.edu>
2024-08-06 15:46:50 -0700

    Narrtive-visuals
    Levels

--- The-1st-Symphony/Assets/Scripts/BouncePad.cs
+++ The-1st-Symphony/Assets/Scripts/BouncePad.cs
@@ -10,7 +10,7 @@ public class BouncePad : MonoBehaviour
     public Vector2 Directions => directions;
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        // if (collision.gameObject.CompareTag("player")) {
+        // if (collision.gameObject.CompareTag("WholeNote")) {
         //     collision.gameObject.GetComponent<Rigidbody2D>().AddForce(directions * bounce, ForceMode2D.Impulse);
         // }
         EventManager.HitBouncePad(collision.gameObject.tag, this);
--- The-1st-Symphony/Assets/Scripts/FallingPlatform.cs
+++ The-1st-Symphony/Assets/Scripts/FallingPlatform.cs
@@ -21,7 +21,7 @@ public class FallingPlatform : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("player"))
+        if (collision.gameObject.CompareTag("WholeNote"))
         {
             StartCoroutine(Fall());
         }
